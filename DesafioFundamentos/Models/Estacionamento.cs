c38bd4c78ea97f81ab42c09c2561601d92fe071f
Guga <guga@Homme.Guga>
2024-01-02 11:04:18 -0300

    feat: remove e dá o preço

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -35,15 +35,19 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // *IMPLEMENTE AQUI*
+                if (int.TryParse(Console.ReadLine(), out int horas))
+                {
+
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
+                    decimal valorTotal = precoInicial + precoPorHora * horas;
                 // TODO: Remover a placa digitada da lista de veículos
+                    veiculos.Remove(placa);
                 // *IMPLEMENTE AQUI*
-
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
+
+
             }
             else
             {
